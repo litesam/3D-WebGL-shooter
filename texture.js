4ea7b3bc2af60fa689b4e6d0d596f13a6f291018
litesam <lightsam126@gmail.com>
2020-04-11 16:18:07 +0530

    Major game fix

--- texture.js
+++ texture.js
@@ -1,17 +1,31 @@
 import { gl } from './game.js';
 
 class Texture {
-	static load = (url, width, height) => {
-		const img = new Image();
-		const texture = gl.createTexture();
-		img.addEventListener('load', function (e) {
-			gl.bindTexture(gl.TEXTURE_2D, texture);
-			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
-			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+	constructor() {
+		this.img = null;
+		this.width = 1;
+		this.height = 1;
+		this.texture = null;
+		this.loaded = false;
+	}
+
+	static load = (url) => {
+		const tex = new Texture();
+		tex.texture = gl.createTexture();
+		gl.bindTexture(gl.TEXTURE_2D, tex.texture);
+		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, tex.width, tex.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, tex.img);	
+		tex.img = new Image();
+		tex.img.addEventListener('load', () => {
+			tex.width = tex.img.width;
+			tex.height = tex.img.height;
+			gl.bindTexture(gl.TEXTURE_2D, tex.texture);
+			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, tex.width, tex.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, tex.img);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
 			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+			tex.loaded = true;
 		});
-		img.src = url;
-		return { img, width: img.width, height: img.height, texture };
+		tex.img.src = url;
+		return tex;
 	}
 }
 
